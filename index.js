15c776ed067f05d115b27c4327c1a994a8110e32
Prerna9646 <prerna9646@gmail.com>
2025-11-14 17:55:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(cors({
+  origin: "*"
+}));
+
 app.get("/health", (req, res) => {
   res.json({ status: "UP", version: "1.0.0" });
 });
